36e84413f5091fd3bfee52b77ff064eae1aeebdd
aveandrian <trancemilly7@gmail.com>
2023-07-19 13:55:27 +0200

    init

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import '../styles/Navbar.css'
+
+export default function Navbar(){
+    const [isOpened, setIsOpened] = useState(false)
+
+    function toggleIsOpened(){
+        setIsOpened(prev => !prev)
+    }
+
+    return (
+        <nav>
+            <div className={`nav-items-bg ${isOpened ? "opened" : ""}`} onClick={toggleIsOpened}></div>
+            <img className='logo' src='/images/logo.svg' />
+            <div className={`nav-items ${isOpened ? "opened" : ""}`}>
+                <a>Pricing</a>
+                <a>Product</a>
+                <a>About Us</a>
+                <a>Careers</a>
+                <a>Community</a>
+            </div>
+            <img className='menu-btn' src={`/images/icon-${isOpened ? "close" : "hamburger"}.svg`} onClick={toggleIsOpened} />
+            <button className='get-started-btn'>Get Started</button>
+        </nav>
+    )
+}
